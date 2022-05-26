63ed0354beb87e7e54173e578d3c37d38c575e54
LT Cloud Bruno <ltcloudbruno@MacBook-Air-de-LT.local>
2022-05-26 18:43:30 -0300

    first initial

---
+++ src/main/java/br/com/contabil/ebcontabil/EbcontabilApplication.java
@@ -0,0 +1,13 @@
+package br.com.contabil.ebcontabil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbcontabilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbcontabilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/contabil/ebcontabil/EbcontabilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.contabil.ebcontabil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbcontabilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
